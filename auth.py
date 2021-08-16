22aa519d2f6d3168a5d54528692684a35c27bacc
Yuri <48300685+Rilufi@users.noreply.github.com>
2021-08-16 15:27:16 -0300

    Update auth.py

--- auth.py
+++ auth.py
@@ -1,5 +1,10 @@
-from key import consumer_key, consumer_secret, access_token, access_token_secret
 import tweepy
+import os
+
+consumer_key = os.environ["CONSUMER_KEY"]
+consumer_secret = os.environ["CONSUMER_SECRET"]
+access_token = os.environ["ACCESS_TOKEN"]
+access_token_secret = os.environ["ACCESS_TOKEN_SECRET"]
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
