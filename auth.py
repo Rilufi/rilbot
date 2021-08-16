99f44f722481b6640d058d870207810817980bf0
Yuri <48300685+Rilufi@users.noreply.github.com>
2021-08-16 15:25:11 -0300

    Add files via upload

---
+++ auth.py
@@ -0,0 +1,8 @@
+from key import consumer_key, consumer_secret, access_token, access_token_secret
+import tweepy
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+# Controls Twitter account
+api = tweepy.API(auth, wait_on_rate_limit = True, wait_on_rate_limit_notify = True)
