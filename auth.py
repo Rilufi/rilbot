3ab75c672db6f40b05259eeeb1cf832886b3217e
Yuri <48300685+Rilufi@users.noreply.github.com>
2021-09-26 16:04:33 -0300

    Update auth.py

--- auth.py
+++ auth.py
@@ -10,4 +10,4 @@ auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 
 # Controls Twitter account
-api = tweepy.API(auth, wait_on_rate_limit = True, wait_on_rate_limit_notify = True)
+api = tweepy.API(auth, wait_on_rate_limit = True)
