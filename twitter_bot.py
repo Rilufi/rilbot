99b7faf91ada13a8795b18f432302e3ba723c477
Yuri <48300685+Rilufi@users.noreply.github.com>
2021-09-27 02:09:44 -0300

    Update twitter_bot.py

--- twitter_bot.py
+++ twitter_bot.py
@@ -8,7 +8,7 @@ import os
 
 # Create class instance
 win = winner()
-#unfollow = unfollow()
+unfollow = unfollow()
 clean_timeline = cleanTimeline()
 
 os.system('cls')
@@ -22,16 +22,14 @@ fortune = os.popen(COMMAND).read()
 #	fortune = os.popen(COMMAND).read()
 
 # Run script
-try:
-	if api.me().friends_count > 3000:
-	    #unfollow.unfollow()
-	    #api.update_status(fortune)
-	elif api.me().favourites_count > 6000 or api.me().statuses_count > 6000:
-	    clean_timeline.unfavorite_unretweet()
-	    #api.update_status(fortune)
-	else:
-	    win.favorite_follow_retweet()
-	    #api.update_status(fortune)
-	print(win.sort_file('twitterFilter.txt') + '\n')
-except:
-	api.update_status(fortune)
+
+if api.me().friends_count > 3000:
+    unfollow.unfollow()
+    #api.update_status(fortune)
+elif api.me().favourites_count > 6000 or api.me().statuses_count > 6000:
+    clean_timeline.unfavorite_unretweet()
+    #api.update_status(fortune)
+else:
+    win.favorite_follow_retweet()
+    #api.update_status(fortune)
+print(win.sort_file('twitterFilter.txt') + '\n')
