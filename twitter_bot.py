ebf7b96f0213ae7553fab19740354089e17f99e3
Yuri <48300685+Rilufi@users.noreply.github.com>
2021-09-27 02:06:44 -0300

    Update twitter_bot.py

--- twitter_bot.py
+++ twitter_bot.py
@@ -25,13 +25,13 @@ fortune = os.popen(COMMAND).read()
 try:
 	if api.me().friends_count > 3000:
 	    #unfollow.unfollow()
-	    api.update_status(fortune)
+	    #api.update_status(fortune)
 	elif api.me().favourites_count > 6000 or api.me().statuses_count > 6000:
 	    clean_timeline.unfavorite_unretweet()
-	    api.update_status(fortune)
+	    #api.update_status(fortune)
 	else:
 	    win.favorite_follow_retweet()
-	    api.update_status(fortune)
+	    #api.update_status(fortune)
 	print(win.sort_file('twitterFilter.txt') + '\n')
 except:
 	api.update_status(fortune)
