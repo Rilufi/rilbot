0a45a68486adeae08af1dff426d775933153718d
Yuri <48300685+Rilufi@users.noreply.github.com>
2021-09-27 02:46:38 -0300

    Update twitter_bot.py

--- twitter_bot.py
+++ twitter_bot.py
@@ -2,41 +2,25 @@
 from auth import api
 from datetime import datetime
 from twitter_clean_timeline import cleanTimeline
-#from twitter_unfollow import unfollow
+from twitter_unfollow import unfollow
 from twitter_winner import winner
 import os
 
 # Create class instance
 win = winner()
-#unfollow = unfollow()
+unfollow = unfollow()
 clean_timeline = cleanTimeline()
 
 os.system('cls')
 
 day = datetime.today().day
 
-# Call fortune
-COMMAND = "fortune"
-fortune = os.popen(COMMAND).read()
-while len(fortune) > 280:
-	fortune = os.popen(COMMAND).read()
-
 # Run script
 
-#if api.me().friends_count > 3000:
-#    unfollow.unfollow()
-    #api.update_status(fortune)
-#elif api.me().favourites_count > 6000 or api.me().statuses_count > 6000:
-#    clean_timeline.unfavorite_unretweet()
-    #api.update_status(fortune)
-#else:
-    #win.favorite_follow_retweet()
-    #api.update_status(fortune)
-    
-try:
+if api.me().friends_count > 3000:
+    unfollow.unfollow()
+elif api.me().favourites_count > 6000 or api.me().statuses_count > 6000:
+    clean_timeline.unfavorite_unretweet()
+else:
     win.favorite_follow_retweet()
-    print(win.sort_file('twitterFilter.txt') + '\n')
-    api.update_status(fortune)
-except:
-    win.favorite_follow_retweet()
-    print(win.sort_file('twitterFilter.txt') + '\n')
+print(win.sort_file('twitterFilter.txt') + '\n')
