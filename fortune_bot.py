073efe139e8ecaa9d0b86bd656d2fa129ebc5239
Yuri <48300685+Rilufi@users.noreply.github.com>
2021-10-03 22:11:58 -0300

    Update fortune_bot.py

--- fortune_bot.py
+++ fortune_bot.py
@@ -6,3 +6,16 @@ fortune = os.popen("fortune fortunes").read()
 while len(fortune) > 280:
 	fortune = os.popen("fortune fortunes").read()
 api.update_status(fortune)
+
+def main():
+    account_name = 'boturitter'
+    for tweet in tweepy.Cursor(api.user_timeline, id=account_name).items(1):
+        try:
+           tweet.favorite()
+           tweet.retweet()
+           print("Tweet Retweeted")
+        except tweepy.TweepError as e:
+           print(e.reason)
+        except StopIteration:
+           break
+main()
