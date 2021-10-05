2da832e1378096404aca5be8b3e0f8d0da73b6bf
Yuri <48300685+Rilufi@users.noreply.github.com>
2021-10-05 01:21:53 -0300

    Update twitter_bot.py

--- twitter_bot.py
+++ twitter_bot.py
@@ -16,10 +16,9 @@ os.system('cls')
 day = datetime.today().day
 
 # Run script
-
-if api.me().friends_count > 3000:
+if day == 1 or api.me().friends_count > 3000:
     unfollow.unfollow()
-elif api.me().favourites_count > 6000 or api.me().statuses_count > 6000:
+elif day in [7, 14, 21, 28] or api.me().favourites_count > 6000 or api.me().statuses_count > 6000:
     clean_timeline.unfavorite_unretweet()
 else:
     win.favorite_follow_retweet()
