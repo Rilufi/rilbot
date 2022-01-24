fd21c7510e90d2fe9256b9279a4c68d9b54eb709
Yuri <48300685+Rilufi@users.noreply.github.com>
2022-01-24 12:31:47 -0300

    Update fortune_bot.py

--- fortune_bot.py
+++ fortune_bot.py
@@ -7,3 +7,10 @@ while len(fortune) > 280:
 	fortune = os.popen("fortune fortunes").read()
 api.update_status(fortune)
 
+tweets = api.user_timeline(screen_name="boturitter", count=1, exclude_replies = True)
+for tweet in tweets:
+	try:
+		tweet.favorite()
+		tweet.retweet()
+	except:
+		pass
