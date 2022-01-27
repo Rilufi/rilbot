3c29cbb470039fd59902d8e3bac343008d739c66
Yuri <48300685+Rilufi@users.noreply.github.com>
2022-01-27 17:09:01 -0300

    Update auth.py

--- auth.py
+++ auth.py
@@ -1,17 +1,6 @@
 import tweepy
 import os
 
-consumer_key = os.environ["CONSUMER_KEY"]
-consumer_secret = os.environ["CONSUMER_SECRET"]
-access_token = os.environ["ACCESS_TOKEN"]
-access_token_secret = os.environ["ACCESS_TOKEN_SECRET"]
-
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
-
-# Controls Twitter account
-api_xame = tweepy.API(auth, wait_on_rate_limit = True)
-
 consumer_key = os.environ["CONSUMER_KEY_RIL"]
 consumer_secret = os.environ["CONSUMER_SECRET_RIL"]
 access_token = os.environ["ACCESS_TOKEN_RIL"]
@@ -23,3 +12,13 @@ auth.set_access_token(access_token, access_token_secret)
 # Controls Twitter account
 api = tweepy.API(auth, wait_on_rate_limit = True)
 
+consumer_key = os.environ["CONSUMER_KEY"]
+consumer_secret = os.environ["CONSUMER_SECRET"]
+access_token = os.environ["ACCESS_TOKEN"]
+access_token_secret = os.environ["ACCESS_TOKEN_SECRET"]
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+# Controls Twitter account
+api_xame = tweepy.API(auth, wait_on_rate_limit = True)
