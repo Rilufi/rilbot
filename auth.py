dfb810f8a776e99c3a985887eb366862f5ee20a9
Yuri <48300685+Rilufi@users.noreply.github.com>
2022-01-27 17:04:51 -0300

    Update auth.py

--- auth.py
+++ auth.py
@@ -10,7 +10,7 @@ auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 
 # Controls Twitter account
-api = tweepy.API(auth, wait_on_rate_limit = True)
+api_xame = tweepy.API(auth, wait_on_rate_limit = True)
 
 consumer_key = os.environ["CONSUMER_KEY_RIL"]
 consumer_secret = os.environ["CONSUMER_SECRET_RIL"]
@@ -21,5 +21,5 @@ auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 
 # Controls Twitter account
-api_ril = tweepy.API(auth, wait_on_rate_limit = True)
+api = tweepy.API(auth, wait_on_rate_limit = True)
 
