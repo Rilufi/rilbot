bb7c4f19618daa0a1163da0f80cbcdafce0e2c65
Yuri <48300685+Rilufi@users.noreply.github.com>
2022-01-27 17:04:18 -0300

    Update fortune_bot.py

--- fortune_bot.py
+++ fortune_bot.py
@@ -1,13 +1,13 @@
-from auth import api
+from auth import api_xame
 import fortune
 import os
 
 fortune = os.popen("fortune fortunes").read()
 while len(fortune) > 280:
 	fortune = os.popen("fortune fortunes").read()
-api.update_status(fortune)
+api_xame.update_status(fortune)
 
-tweets = api.user_timeline(screen_name="boturitter", count=1, exclude_replies = True)
+tweets = api_xame.user_timeline(screen_name="boturitter", count=1, exclude_replies = True)
 for tweet in tweets:
 	try:
 		tweet.favorite()
