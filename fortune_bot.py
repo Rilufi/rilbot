2ce7d7407c3e7af92ae2f501f9a624e1d0d0b267
Yuri <48300685+Rilufi@users.noreply.github.com>
2022-03-18 17:02:32 -0300

    Update fortune_bot.py

--- fortune_bot.py
+++ fortune_bot.py
@@ -7,13 +7,36 @@ from auth import api_xame, api_uva, api_mevu, api_ril, api_zark
 bots = [api_xame, api_uva, api_mevu, api_ril, api_zark]
 coun = 0
 
-def fort(api):
+
+def get_chunks(s, maxlength):
+    start = 0
+    end = 0
+    while start + maxlength  < len(s) and end != -1:
+        end = s.rfind(" ", start, start + maxlength + 1)
+        yield s[start:end]
+        start = end +1
+    yield s[start:]
+
+def une(api):
 	fortune = os.popen("fortune fortunes").read()
-	while len(fortune) > 280:
-		fortune = os.popen("fortune fortunes").read()
+	if len(fortune) < 280:
 		api.update_status(fortune)
-		coun+=1
-		print("{coun} fortuna postada")
+	elif len(fortune) > 280:
+		chunks = get_chunks(fortune, 280)
+
+		chunkex = [(n) for n in chunks]
+
+		coun = 0
+
+		while coun < len(chunkex):
+    			tweets = api.user_timeline(screen_name = api.me().screen_name, count=1)
+    			for tweet in tweets:
+        			api.update_status(str(chunkex[coun]), in_reply_to_status_id = tweet.id, auto_populate_reply_metadata = True)
+        			coun += 1
+	else:
+		pass
 		
 for bot in bots:
-	fort(bot)
+	une(bot)
+	coun+=1
+	print(f"{coun} fortuna postada")
