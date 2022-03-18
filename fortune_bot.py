bb7f45a6153cbaa25bca1db8b328e695150b67dc
Yuri <48300685+Rilufi@users.noreply.github.com>
2022-03-18 13:49:44 -0300

    Update fortune_bot.py

--- fortune_bot.py
+++ fortune_bot.py
@@ -5,12 +5,15 @@ from auth import api_xame, api_uva, api_mevu, api_ril, api_zark
 
 
 bots = [api_xame, api_uva, api_mevu, api_ril, api_zark]
+coun = 0
 
 def fort(api):
 	fortune = os.popen("fortune fortunes").read()
 	while len(fortune) > 280:
 		fortune = os.popen("fortune fortunes").read()
 		api.update_status(fortune)
-
+		coun+=1
+		print("{coun} fortuna postada")
+		
 for bot in bots:
 	fort(bot)
