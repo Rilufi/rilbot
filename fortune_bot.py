4b228b79f6de63426957c758c919b29e371bd078
Yuri <48300685+Rilufi@users.noreply.github.com>
2022-03-18 13:11:00 -0300

    Update fortune_bot.py

--- fortune_bot.py
+++ fortune_bot.py
@@ -1,29 +1,16 @@
 import tweepy
 import fortune
 import os
+from auth import api_xame, api_uva, api_mevu, api_ril, api_zark
 
 
-consumer_key = os.environ["CONSUMER_KEY"]
-consumer_secret = os.environ["CONSUMER_SECRET"]
-access_token = os.environ["ACCESS_TOKEN"]
-access_token_secret = os.environ["ACCESS_TOKEN_SECRET"]
+bots = [api_xame, api_uva, api_mevu, api_ril, api_zark]
 
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
-
-# Controls Twitter account
-api_xame = tweepy.API(auth, wait_on_rate_limit = True)
-
-
-fortune = os.popen("fortune fortunes").read()
-while len(fortune) > 280:
+def fort(api):
 	fortune = os.popen("fortune fortunes").read()
-api_xame.update_status(fortune)
+	while len(fortune) > 280:
+		fortune = os.popen("fortune fortunes").read()
+		api.update_status(fortune)
 
-tweets = api_xame.user_timeline(screen_name="boturitter", count=1, exclude_replies = True)
-for tweet in tweets:
-	try:
-		tweet.favorite()
-		tweet.retweet()
-	except:
-		pass
+for bot in bots:
+	fort(bot)
