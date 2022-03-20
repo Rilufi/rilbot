c603b67f13b433d20b88b2ceff4b1b792036ff89
Yuri <48300685+Rilufi@users.noreply.github.com>
2022-03-20 01:45:08 -0300

    Update twitter_bot.py

--- alts/xame/twitter_bot.py
+++ alts/xame/twitter_bot.py
@@ -15,8 +15,8 @@ day = datetime.today().day
 # Run script
 if day == 3 or api.me().friends_count > 3000:
     unfollow.unfollow()
-#elif day in [5, 12, 19, 26] or api.me().favourites_count > 6000 or api.me().statuses_count > 6000:
-#    clean_timeline.unfavorite_unretweet()
+elif day in [5, 12, 19, 26] or api.me().favourites_count > 6000 or api.me().statuses_count > 6000:
+    clean_timeline.unfavorite_unretweet()
 else:
     win.favorite_follow_retweet()
 print(win.sort_file('twitterFilter.txt') + '\n')
