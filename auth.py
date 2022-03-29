7e76930727e3df6371e685de0f84bd6bceef7312
Yuri <48300685+Rilufi@users.noreply.github.com>
2022-03-29 14:54:40 -0300

    Update auth.py

--- auth.py
+++ auth.py
@@ -12,7 +12,7 @@ auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 
 # Controls Twitter account
-api = tweepy.API(auth, wait_on_rate_limit = True, wait_on_rate_limit_notify = True, timeout = 120)
+api = tweepy.API(auth, wait_on_rate_limit = True, wait_on_rate_limit_notify = True, timeout = 1000)
 
 
 #Xamexavu
