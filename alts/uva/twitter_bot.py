c486d932bfbaf5bfefe89ec703e328f846d29c20
Yuri <48300685+Rilufi@users.noreply.github.com>
2022-03-31 12:45:27 -0300

    Update twitter_bot.py

--- alts/uva/twitter_bot.py
+++ alts/uva/twitter_bot.py
@@ -14,7 +14,7 @@ clean_timeline = cleanTimeline()
 day = datetime.today().day
 
 # Run script
-if day == 1 or api.me().friends_count > 3000:
+if day == 31 and api.me().friends_count > 3000:
     unfollow.unfollow()
 elif day in [7, 14, 21, 28] or api.me().favourites_count > 6000 or api.me().statuses_count > 6000:
     clean_timeline.unfavorite_unretweet()
