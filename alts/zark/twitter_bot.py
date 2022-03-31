683588ae804bd7b1f05a08594da110136a6fd465
Yuri <48300685+Rilufi@users.noreply.github.com>
2022-03-31 20:19:02 -0300

    Update twitter_bot.py

--- alts/zark/twitter_bot.py
+++ alts/zark/twitter_bot.py
@@ -14,7 +14,10 @@ day = datetime.today().day
 
 # Run script
 if day == 2 or api.me().friends_count > 3000:
-    unfollow.unfollow()
+    try:
+        unfollow.unfollow()
+    except:
+        pass
 elif day in [6, 13, 20, 27] or api.me().favourites_count > 6000 or api.me().statuses_count > 6000:
     clean_timeline.unfavorite_unretweet()
 else:
