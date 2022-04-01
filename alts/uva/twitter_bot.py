aa6e16a25f1e023bff6cb7330258ebac9c6ac8d4
Yuri <48300685+Rilufi@users.noreply.github.com>
2022-04-01 15:44:38 -0300

    Update twitter_bot.py

--- alts/uva/twitter_bot.py
+++ alts/uva/twitter_bot.py
@@ -13,14 +13,16 @@ clean_timeline = cleanTimeline()
 
 day = datetime.today().day
 
+clean_timeline.unfavorite_unretweet()
+
 # Run script
-if day == 1 or api.me().friends_count > 3000:
-    try:
-        unfollow.unfollow()
-    except:
-        pass
-elif day in [7, 14, 21, 28] or api.me().favourites_count > 6000 or api.me().statuses_count > 6000:
-    clean_timeline.unfavorite_unretweet()
-else:
-    win.favorite_follow_retweet()
+#if day == 1 or api.me().friends_count > 3000:
+#    try:
+#        unfollow.unfollow()
+#    except:
+#        pass
+#elif day in [7, 14, 21, 28] or api.me().favourites_count > 6000 or api.me().statuses_count > 6000:
+#    clean_timeline.unfavorite_unretweet()
+#else:
+#    win.favorite_follow_retweet()
 print(win.sort_file('twitterFilter.txt') + '\n')
