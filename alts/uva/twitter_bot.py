168d9b00c1cf4c7b2408c1039f86b1221ac01361
Yuri <48300685+Rilufi@users.noreply.github.com>
2022-05-17 20:09:08 -0300

    Update twitter_bot.py

--- alts/uva/twitter_bot.py
+++ alts/uva/twitter_bot.py
@@ -1,23 +1,18 @@
 # Run: py twitter_bot.py
 from auth import api
 from datetime import datetime
-from twitter_clean_timeline import cleanTimeline
-from twitter_unfollow import unfollow
 from twitter_winner import winner
 
 # Create class instance
 win = winner()
-unfollow = unfollow()
-clean_timeline = cleanTimeline()
 
 day = datetime.today().day
 
 # Run script
-#if day == 1 or api.me().friends_count > 3000:
-#    unfollow.unfollow()
-#elif day in [7, 14, 21, 28] or api.me().favourites_count > 6000 or api.me().statuses_count > 6000:
-#    clean_timeline.unfavorite_unretweet()
-#else:
-win.favorite_follow_retweet()
+if day in [1, 7, 14, 21, 28]
+  print("Uvaxemax is on vacation.")
+  pass
+else:
+  win.favorite_follow_retweet()
 
 print(win.sort_file('twitterFilter.txt') + '\n')
