0c3c0089411956bde3624381bd36159450e63897
Yuri <48300685+Rilufi@users.noreply.github.com>
2022-05-17 20:13:44 -0300

    Update twitter_bot.py

--- alts/zark/twitter_bot.py
+++ alts/zark/twitter_bot.py
@@ -1,23 +1,18 @@
 # Run: py twitter_bot.py
 from auth import api
 from datetime import datetime
-from twitter_clean_timeline import cleanTimeline
-from twitter_unfollow import unfollow
 from twitter_winner import winner
 
 # Create class instance
 win = winner()
-unfollow = unfollow()
-clean_timeline = cleanTimeline()
 
 day = datetime.today().day
 
 # Run script
-#if day == 2 or api.me().friends_count > 3000:
-#    unfollow.unfollow()
-#elif day in [6, 13, 20, 27] or api.me().favourites_count > 6000 or api.me().statuses_count > 6000:
-#    clean_timeline.unfavorite_unretweet()
-#else:
-win.favorite_follow_retweet()
+if day in [2, 6, 13, 20, 27]:
+  print("Zarknall is nowhere to be found.")
+  pass
+else:
+  win.favorite_follow_retweet()
 
 print(win.sort_file('twitterFilter.txt') + '\n')
