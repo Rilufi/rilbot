5b5e320a72d98c68a376a6787e80b45e079b1911
Yuri <48300685+Rilufi@users.noreply.github.com>
2022-05-17 20:03:20 -0300

    Update twitter_bot.py

--- twitter_bot.py
+++ twitter_bot.py
@@ -13,11 +13,10 @@ clean_timeline = cleanTimeline()
 day = datetime.today().day
 
 # Run script
-#if day == 4 or api.me().friends_count > 3000:
-#    unfollow.unfollow()
-#elif day in [8, 11, 18, 25] or api.me().favourites_count > 6000 or api.me().statuses_count > 6000:
-#    clean_timeline.unfavorite_unretweet()
-#else:
-win.favorite_follow_retweet()
+if day in [4, 8, 11, 18, 25]:
+  print("Rilufix not today, Satan")
+  pass
+else:
+  win.favorite_follow_retweet()
 
 print(win.sort_file('twitterFilter.txt') + '\n')
