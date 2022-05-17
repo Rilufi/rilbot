affae7f1ff1a6852936a1434a4ede5668af31a67
Yuri <48300685+Rilufi@users.noreply.github.com>
2022-05-17 20:06:25 -0300

    Update twitter_bot.py

--- twitter_bot.py
+++ twitter_bot.py
@@ -1,20 +1,16 @@
 # Run: py twitter_bot.py
 from auth import api
 from datetime import datetime
-from twitter_clean_timeline import cleanTimeline
-from twitter_unfollow import unfollow
 from twitter_winner import winner
 
 # Create class instance
 win = winner()
-unfollow = unfollow()
-clean_timeline = cleanTimeline()
 
 day = datetime.today().day
 
 # Run script
 if day in [4, 8, 11, 18, 25]:
-  print("Rilufix not today, Satan")
+  print("Rilufix not today, Satan.")
   pass
 else:
   win.favorite_follow_retweet()
