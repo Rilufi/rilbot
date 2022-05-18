cb8a11bd9b7c65138599fd9e4bf49caf9162237b
Yuri <48300685+Rilufi@users.noreply.github.com>
2022-05-17 23:00:37 -0300

    Update auth.py

--- auth.py
+++ auth.py
@@ -74,3 +74,20 @@ auth.set_access_token(access_token, access_token_secret)
 
 # Controls Twitter account
 api_zark = tweepy.API(auth, wait_on_rate_limit = True, wait_on_rate_limit_notify=True) 
+
+
+
+#Lufilir
+
+
+consumer_key = os.environ["CONSUMER_KEY_LUFI"]
+consumer_secret = os.environ["CONSUMER_SECRET_LUFI"]
+access_token = os.environ["ACCESS_TOKEN_LUFI"]
+access_token_secret = os.environ["ACCESS_TOKEN_SECRET_LUFI"]
+
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+# Controls Twitter account
+api_lufi = tweepy.API(auth, wait_on_rate_limit = True, wait_on_rate_limit_notify=True) 
