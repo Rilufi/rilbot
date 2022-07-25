3905401dc4548c3bab6c00ba6d012998445c4b7d
Yuri <48300685+Rilufi@users.noreply.github.com>
2022-07-25 09:27:24 -0300

    Update twitter_bot.py

--- alts/mevu/twitter_bot.py
+++ alts/mevu/twitter_bot.py
@@ -2,16 +2,18 @@
 from auth import api
 from datetime import datetime
 from twitter_winner import winner
+from twitter_unfollow import desfollow
 
 # Create class instance
 win = winner()
+desf = desfollow()
 
 day = datetime.today().day
 
 # Run script
 if day in [5, 9, 15, 23, 30]:
   print("Mevuxa is not home today.")
-  pass
+  desf.unfollower()
 else:
   win.favorite_follow_retweet()
 
