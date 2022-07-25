945ebb3befec011b9c907243742b508acc4c5560
Yuri <48300685+Rilufi@users.noreply.github.com>
2022-07-25 12:19:19 -0300

    Update twitter_bot.py

--- alts/uva/twitter_bot.py
+++ alts/uva/twitter_bot.py
@@ -2,18 +2,18 @@
 from auth import api
 from datetime import datetime
 from twitter_winner import winner
-from twitter_unfollow import desfollow
+from twitter_unfollow import unfollow
 
 # Create class instance
 win = winner()
-desf = desfollow()
+unfollow = unfollow()
 
 day = datetime.today().day
 
 # Run script
 if day in [1, 7, 14, 21, 28]:
   print("Uvaxemax is on vacation.")
-  desf.unfollower()
+  unfollow.unfollow()
 else:
   win.favorite_follow_retweet()
 
