8f1e3987cb320aad95e363f5cd1cdcac0154781a
Yuri <48300685+Rilufi@users.noreply.github.com>
2022-07-28 16:49:26 -0300

    Update twitter_bot.py

--- alts/uva/twitter_bot.py
+++ alts/uva/twitter_bot.py
@@ -15,10 +15,10 @@ day = datetime.today().day
 # Run script
 if day in [1, 7, 14, 21, 28]:
   print("Uva is on vacation.")
+  unfollow.unfollow()
   tweetbot.read('Freud_Dream_Psychology.txt')
   my_first_text = tweetbot.generate_text(25, seedword=[u'dream', u'psychoanalysis'])
   api.update_status('"'+my_first_text+'"')
-  unfollow.unfollow()
 else:
   win.favorite_follow_retweet()
 
