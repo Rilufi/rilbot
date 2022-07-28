fe5a08efe94d558b9fd5bec7c1c6b0cb598bc6a8
Yuri <48300685+Rilufi@users.noreply.github.com>
2022-07-28 11:13:56 -0300

    Update twitter_bot.py

--- alts/zark/twitter_bot.py
+++ alts/zark/twitter_bot.py
@@ -3,16 +3,19 @@ from auth import api
 from datetime import datetime
 from twitter_winner import winner
 from twitter_unfollow import unfollow
+from zark import quoter
 
 # Create class instance
 win = winner()
 unfollow = unfollow()
+quoter = quoter()
 
 day = datetime.today().day
 
 # Run script
 if day in [2, 11, 16, 20, 27]:
   print("Zarknall is nowhere to be found.")
+  quoter.tweet_quote()
   unfollow.unfollow()
 else:
   win.favorite_follow_retweet()
