f8c4644b351a0aedc4821a4778437b2c22fed66a
Yuri <48300685+Rilufi@users.noreply.github.com>
2022-07-28 15:30:49 -0300

    Update twitter_bot.py

--- alts/uva/twitter_bot.py
+++ alts/uva/twitter_bot.py
@@ -3,16 +3,21 @@ from auth import api
 from datetime import datetime
 from twitter_winner import winner
 from twitter_unfollow import unfollow
+from uva import MarkovBot
 
 # Create class instance
 win = winner()
 unfollow = unfollow()
+tweetbot = MarkovBot()
 
 day = datetime.today().day
 
 # Run script
 if day in [1, 7, 14, 21, 28]:
-  print("Uvaxemax is on vacation.")
+  print("Uva is on vacation.")
+  tweetbot.read('Freud_Dream_Psychology.txt')
+  my_first_text = tweetbot.generate_text(25, seedword=[u'dream', u'psychoanalysis'])
+  api.update_status('"'+my_first_text+'"')
   unfollow.unfollow()
 else:
   win.favorite_follow_retweet()
