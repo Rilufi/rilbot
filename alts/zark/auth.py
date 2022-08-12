43359681eee53e2f3f889ee456f0ec5f24555268
Yuri <48300685+Rilufi@users.noreply.github.com>
2022-08-12 11:00:15 -0300

    Update auth.py

--- alts/zark/auth.py
+++ alts/zark/auth.py
@@ -14,4 +14,4 @@ auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 
 # Controls Twitter account
-api = tweepy.API(auth, wait_on_rate_limit = True, wait_on_rate_limit_notify=True, timeout = 1000) 
+api = tweepy.API(auth, wait_on_rate_limit = True)
