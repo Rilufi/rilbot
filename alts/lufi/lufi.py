30685e497676f8182dc9a021be232528fae3aafa
Yuri <48300685+Rilufi@users.noreply.github.com>
2022-08-12 12:49:51 -0300

    Create lufi.py

---
+++ alts/lufi/lufi.py
@@ -0,0 +1,28 @@
+import json
+import random
+from auth import api
+
+def get_quotes():
+    with open('got.json') as f:
+        quotes_json = json.load(f)
+    return quotes_json['quotes']
+
+def get_random_quote():
+    quotes = get_quotes()
+    random_quote = random.choice(quotes)
+    return random_quote
+
+def create_tweet():
+    quote = get_random_quote()
+    tweet = """
+            "{}"
+            -{}
+            """.format(quote['quote'], quote['character'])
+    return tweet
+
+class quoter:
+    def tweet_quote(self):
+        print('getting a random quote...')        
+        tweet = create_tweet()
+        api.update_status(tweet)
+        print(tweet)
