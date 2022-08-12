f6486d15e76a278e992d86c2c8046fedbf48417d
Yuri <48300685+Rilufi@users.noreply.github.com>
2022-08-12 10:59:21 -0300

    Update auth.py

--- alts/xame/auth.py
+++ alts/xame/auth.py
@@ -11,4 +11,4 @@ access_token_secret = os.environ["ACCESS_TOKEN_SECRET_XAME"]
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 
-api = tweepy.API(auth, wait_on_rate_limit = True, wait_on_rate_limit_notify = True, timeout = 1000)
+api = tweepy.API(auth, wait_on_rate_limit = True)
