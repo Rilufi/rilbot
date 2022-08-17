1cb78033f65470b43e9d3e699cab38f8d9624bfb
Yuri <48300685+Rilufi@users.noreply.github.com>
2022-08-17 17:32:13 -0300

    Update auth.py

--- auth.py
+++ auth.py
@@ -47,7 +47,7 @@ api_uva = tweepy.API(auth, wait_on_rate_limit = True)
 consumer_key = os.environ["CONSUMER_KEY_MEVU"]
 consumer_secret = os.environ["CONSUMER_SECRET_MEVU"]
 access_token = os.environ["ACCESS_TOKEN_MEVU"]
-access_token_secret = os.environ["ACCESS_TOKEN_SECRET_M
+access_token_secret = os.environ["ACCESS_TOKEN_SECRET_MEVU"]
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
