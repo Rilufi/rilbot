ab43a95f93f6fddd57e22d2378c4bdc33eb17c65
Yuri <48300685+Rilufi@users.noreply.github.com>
2022-09-27 23:17:49 -0300

    Update twitter_bot.py

--- alts/xame/twitter_bot.py
+++ alts/xame/twitter_bot.py
@@ -3,16 +3,19 @@ from auth import api
 from datetime import datetime
 from twitter_winner import winner
 from twitter_unfollow import unfollow
+from xame import rtquery
 
 # Create class instance
 win = winner()
 unfollow = unfollow()
+rt = rtquery()
 
 day = datetime.today().day
 
 # Run script
 if day in [3, 10, 17, 22, 26]:
   print("Xamexavu is meditating today.")
+  rt.rtquery()
 #  unfollow.unfollow()
 else:
   win.favorite_follow_retweet()
