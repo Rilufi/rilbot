43e42162215f3a39fa4da0527d5a558c0913f43a
Yuri <48300685+Rilufi@users.noreply.github.com>
2022-09-28 11:46:54 -0300

    Update fortune_bot.py

--- fortune_bot.py
+++ fortune_bot.py
@@ -5,15 +5,11 @@ from auth import api_xame, api_uva, api_mevu, api, api_zark, api_lufi, api_woba
 
 
 bots = [api_xame, api_uva, api_mevu, api, api_zark, api_lufi, api_woba]
-coun = 0
 
 def une(api):
 	fortune = os.popen("fortune alts/fortunes").read()
 	while len(fortune) < 280:
 		api.update_status(fortune)
-		coun += 1
 		
 for bot in bots:
 	une(bot)
-	coun+=1
-	print(f"{coun} fortuna postada")
