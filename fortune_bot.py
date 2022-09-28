f9534120453ba410e550da48f36b4b016ac3bcd4
Yuri <48300685+Rilufi@users.noreply.github.com>
2022-09-28 11:38:10 -0300

    Update fortune_bot.py

--- fortune_bot.py
+++ fortune_bot.py
@@ -1,10 +1,10 @@
 import tweepy
 import fortune
 import os
-from auth import api_xame, api_uva, api_mevu, api, api_zark, api_lufi
+from auth import api_xame, api_uva, api_mevu, api, api_zark, api_lufi, api_woba
 
 
-bots = [api_xame, api_uva, api_mevu, api, api_zark, api_lufi]
+bots = [api_xame, api_uva, api_mevu, api, api_zark, api_lufi, api_woba]
 coun = 0
 
 def une(api):
