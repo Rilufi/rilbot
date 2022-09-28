ba6df5ba13ea9e8787eae57e0817c722fafa06a2
Yuri <48300685+Rilufi@users.noreply.github.com>
2022-09-28 11:37:29 -0300

    Update auth.py

--- auth.py
+++ auth.py
@@ -82,3 +82,17 @@ auth.set_access_token(access_token, access_token_secret)
 
 # Controls Twitter account
 api_lufi = tweepy.API(auth, wait_on_rate_limit = True)
+
+
+#Wobafab
+
+consumer_key = os.environ["CONSUMER_KEY_WOBA"]
+consumer_secret = os.environ["CONSUMER_SECRET_WOBA"]
+access_token = os.environ["ACCESS_TOKEN_WOBA"]
+access_token_secret = os.environ["ACCESS_TOKEN_SECRET_WOBA"]
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+# Controls Twitter account
+api_woba = tweepy.API(auth, wait_on_rate_limit = True)
