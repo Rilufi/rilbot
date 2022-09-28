a3c03b217767f5d2c5355a1e106c794fbb503a27
Yuri <48300685+Rilufi@users.noreply.github.com>
2022-09-28 11:50:27 -0300

    Update mevu.py

--- alts/mevu/mevu.py
+++ alts/mevu/mevu.py
@@ -3,7 +3,7 @@ import random
 
 class fact:
   def fact(self):
-    lines=open('Facts.txt').read().splitlines()
+    lines=open('alts/Facts.txt').read().splitlines()
     print(lines)
     status = random.choice(lines)
     api.update_status(status)
