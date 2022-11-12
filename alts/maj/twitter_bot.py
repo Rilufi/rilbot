90223be990f36b9329dfb0578127e29d543a80ed
Yuri <48300685+Rilufi@users.noreply.github.com>
2022-11-12 10:06:33 -0300

    Update twitter_bot.py

--- alts/maj/twitter_bot.py
+++ alts/maj/twitter_bot.py
@@ -2,21 +2,17 @@
 from auth import api
 from datetime import datetime
 from twitter_winner import winner
-from twitter_unfollow import unfollow
 from maj import rtquery
 
 # Create class instance
 win = winner()
-unfollow = unfollow()
 rt = rtquery()
-
 day = datetime.today().day
 
 # Run script
-if day in [5, 12, 17, 26, 30]:
+if day in [3, 8, 13, 18, 23, 28]:
   print("Majin is in a really rage right now.")
   rt.rtquery()
-#  unfollow.unfollow()
 else:
   win.favorite_follow_retweet()
 
