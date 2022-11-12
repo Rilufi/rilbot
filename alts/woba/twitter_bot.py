84e4eaa041c194b5a95993c4264e51ce3c25d4c3
Yuri <48300685+Rilufi@users.noreply.github.com>
2022-11-12 10:05:17 -0300

    Update twitter_bot.py

--- alts/woba/twitter_bot.py
+++ alts/woba/twitter_bot.py
@@ -2,21 +2,17 @@
 from auth import api
 from datetime import datetime
 from twitter_winner import winner
-from twitter_unfollow import unfollow
 from woba import rtquery
 
 # Create class instance
 win = winner()
-unfollow = unfollow()
 rt = rtquery()
-
 day = datetime.today().day
 
 # Run script
-if day in [1, 10, 15, 20, 25]:
+if day in [2, 7, 12, 17, 22, 27]:
   print("Wobafab is training for a wrestling tournament.")
   rt.rtquery()
-#  unfollow.unfollow()
 else:
   win.favorite_follow_retweet()
 
