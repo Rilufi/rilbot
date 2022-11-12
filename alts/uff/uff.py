8403dbe96b08fca8a43073a6dc7c141dc981c121
Yuri <48300685+Rilufi@users.noreply.github.com>
2022-11-12 10:13:06 -0300

    Update uff.py

--- alts/uff/uff.py
+++ alts/uff/uff.py
@@ -4,7 +4,7 @@ from auth import api
 
 class rtquery:
     def rtquery(self):
-        for tweet in tweepy.Cursor(api.search, q=f"Zelda -filter:retweets -filter:replies filter:images", result_type="popular").items(1):
+        for tweet in tweepy.Cursor(api.search, q=f"Cryptocurrency -filter:retweets -filter:replies filter:images", result_type="popular").items(3):
             try:
                 api.create_favorite(tweet.id)
                 tweet.retweet()
