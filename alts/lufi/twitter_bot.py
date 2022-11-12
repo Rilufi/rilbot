93f99d6274db43a63a81ef72734d25c3b0e90d45
Yuri <48300685+Rilufi@users.noreply.github.com>
2022-11-12 09:56:14 -0300

    Update twitter_bot.py

--- alts/lufi/twitter_bot.py
+++ alts/lufi/twitter_bot.py
@@ -2,20 +2,16 @@
 from auth import api
 from datetime import datetime
 from twitter_winner import winner
-from twitter_unfollow import unfollow
 from lufi import quoter
 
 # Create class instance
 win = winner()
-unfollow = unfollow()
 quoter = quoter()
-
 day = datetime.today().day
 
 # Run script
-if day in [6, 12, 18, 24, 29]:
+if day in [2, 7, 12, 17, 22, 27]:
   print("Lufilir don't work today.")
-#  unfollow.unfollow()
   quoter.tweet_quote()
 else:
   win.favorite_follow_retweet()
