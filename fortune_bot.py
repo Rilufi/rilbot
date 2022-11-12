90ea8d32dac6ae18c03e2d6c4e9578f43e9aaed7
Yuri <48300685+Rilufi@users.noreply.github.com>
2022-11-12 10:31:08 -0300

    Update fortune_bot.py

--- fortune_bot.py
+++ fortune_bot.py
@@ -1,10 +1,10 @@
 import tweepy
 import fortune
 import os
-from auth import api_xame, api_uva, api_mevu, api, api_zark, api_lufi, api_woba, api_maj, api_zeld
+from auth import api_xame, api_uva, api_mevu, api, api_zark, api_lufi, api_woba, api_maj, api_zeld, api_uff
 
 
-bots = [api_xame, api_uva, api_mevu, api, api_zark, api_lufi, api_woba, api_maj, api_zeld]
+bots = [api_xame, api_uva, api_mevu, api, api_zark, api_lufi, api_woba, api_maj, api_zeld, api_uff]
 
 def une(api):
 	fortune = os.popen("fortune alts/fortunes").read()
