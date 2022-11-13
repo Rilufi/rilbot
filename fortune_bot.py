24386930cc669991481784f29116738ea1e2c420
Yuri <48300685+Rilufi@users.noreply.github.com>
2022-11-13 17:49:54 -0300

    Update fortune_bot.py

--- fortune_bot.py
+++ fortune_bot.py
@@ -12,10 +12,10 @@ def une(api):
 #	while len(fortune) < 280:
 	if len(fortune) < 280:
 		api.update_status(fortune)
-                print(f"fortuna p/ {my_screen_name} rolou")
+		print(f"fortuna p/ {my_screen_name} rolou")
 	else:
 		print(f"sem fortuna p/ {my_screen_name} hoje")
-                pass
+		pass
 		
 for bot in bots:
 	une(bot)
