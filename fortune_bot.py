ce9d80d43bedd684ddbc0e8b4f59cc486efe98fb
Yuri <48300685+Rilufi@users.noreply.github.com>
2022-11-13 17:46:34 -0300

    Update fortune_bot.py

--- fortune_bot.py
+++ fortune_bot.py
@@ -8,11 +8,14 @@ bots = [api_xame, api_uva, api_mevu, api, api_zark, api_lufi, api_woba, api_maj,
 
 def une(api):
 	fortune = os.popen("fortune alts/fortunes").read()
+        my_screen_name = api.me().screen_name
 #	while len(fortune) < 280:
 	if len(fortune) < 280:
 		api.update_status(fortune)
+                print(f"fortuna p/ {my_screen_name} rolou")
 	else:
-		pass
+		print(f"sem fortuna p/ {my_screen_name} hoje")
+                pass
 		
 for bot in bots:
 	une(bot)
