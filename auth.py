a8f9bf6a81ad260a42286bcfb02a217f96c0d09b
Yuri <48300685+Rilufi@users.noreply.github.com>
2022-11-14 10:04:03 -0300

    Update auth.py

--- auth.py
+++ auth.py
@@ -3,10 +3,10 @@ import os
 
 #Ril
 
-consumer_key = os.environ["CONSUMER_KEY_RIL"]
-consumer_secret = os.environ["CONSUMER_SECRET_RIL"]
-access_token = os.environ["ACCESS_TOKEN_RIL"]
-access_token_secret = os.environ["ACCESS_TOKEN_SECRET_RIL"]
+consumer_key = os.environ.get("CONSUMER_KEY_RIL")
+consumer_secret = os.environ.get("CONSUMER_SECRET_RIL")
+access_token = os.environ.get("ACCESS_TOKEN_RIL")
+access_token_secret = os.environ.get("ACCESS_TOKEN_SECRET_RIL")
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
@@ -17,10 +17,10 @@ api = tweepy.API(auth, wait_on_rate_limit = True)
 
 #Xame
 
-consumer_key = os.environ["CONSUMER_KEY_XAME"]
-consumer_secret = os.environ["CONSUMER_SECRET_XAME"]
-access_token = os.environ["ACCESS_TOKEN_XAME"]
-access_token_secret = os.environ["ACCESS_TOKEN_SECRET_XAME"]
+consumer_key = os.environ.get("CONSUMER_KEY_XAME")
+consumer_secret = os.environ.get("CONSUMER_SECRET_XAME")
+access_token = os.environ.get("ACCESS_TOKEN_XAME")
+access_token_secret = os.environ.get("ACCESS_TOKEN_SECRET_XAME")
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
@@ -30,10 +30,10 @@ api_xame = tweepy.API(auth, wait_on_rate_limit = True)
 
 #Uva
 
-consumer_key = os.environ["CONSUMER_KEY_UVA"]
-consumer_secret = os.environ["CONSUMER_SECRET_UVA"]
-access_token = os.environ["ACCESS_TOKEN_UVA"]
-access_token_secret = os.environ["ACCESS_TOKEN_SECRET_UVA"]
+consumer_key = os.environ.get("CONSUMER_KEY_UVA")
+consumer_secret = os.environ.get("CONSUMER_SECRET_UVA")
+access_token = os.environ.get("ACCESS_TOKEN_UVA")
+access_token_secret = os.environ.get("ACCESS_TOKEN_SECRET_UVA")
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
@@ -44,10 +44,10 @@ api_uva = tweepy.API(auth, wait_on_rate_limit = True)
 
 #Mevu
 
-consumer_key = os.environ["CONSUMER_KEY_MEVU"]
-consumer_secret = os.environ["CONSUMER_SECRET_MEVU"]
-access_token = os.environ["ACCESS_TOKEN_MEVU"]
-access_token_secret = os.environ["ACCESS_TOKEN_SECRET_MEVU"]
+consumer_key = os.environ.get("CONSUMER_KEY_MEVU")
+consumer_secret = os.environ.get("CONSUMER_SECRET_MEVU")
+access_token = os.environ.get("ACCESS_TOKEN_MEVU")
+access_token_secret = os.environ.get("ACCESS_TOKEN_SECRET_MEVU")
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
@@ -58,10 +58,10 @@ api_mevu = tweepy.API(auth, wait_on_rate_limit = True)
 
 #Zark
 
-consumer_key = os.environ["CONSUMER_KEY_ZARK"]
-consumer_secret = os.environ["CONSUMER_SECRET_ZARK"]
-access_token = os.environ["ACCESS_TOKEN_ZARK"]
-access_token_secret = os.environ["ACCESS_TOKEN_SECRET_ZARK"]
+consumer_key = os.environ.get("CONSUMER_KEY_ZARK")
+consumer_secret = os.environ.get("CONSUMER_SECRET_ZARK")
+access_token = os.environ.get("ACCESS_TOKEN_ZARK")
+access_token_secret = os.environ.get("ACCESS_TOKEN_SECRET_ZARK")
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
@@ -72,10 +72,10 @@ api_zark = tweepy.API(auth, wait_on_rate_limit = True)
 
 #Lufi
 
-consumer_key = os.environ["CONSUMER_KEY_LUFI"]
-consumer_secret = os.environ["CONSUMER_SECRET_LUFI"]
-access_token = os.environ["ACCESS_TOKEN_LUFI"]
-access_token_secret = os.environ["ACCESS_TOKEN_SECRET_LUFI"]
+consumer_key = os.environ.get("CONSUMER_KEY_LUFI")
+consumer_secret = os.environ.get("CONSUMER_SECRET_LUFI")
+access_token = os.environ.get("ACCESS_TOKEN_LUFI")
+access_token_secret = os.environ.get("ACCESS_TOKEN_SECRET_LUFI")
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
@@ -86,10 +86,10 @@ api_lufi = tweepy.API(auth, wait_on_rate_limit = True)
 
 #Woba
 
-consumer_key = os.environ["CONSUMER_KEY_WOBA"]
-consumer_secret = os.environ["CONSUMER_SECRET_WOBA"]
-access_token = os.environ["ACCESS_TOKEN_WOBA"]
-access_token_secret = os.environ["ACCESS_TOKEN_SECRET_WOBA"]
+consumer_key = os.environ.get("CONSUMER_KEY_WOBA")
+consumer_secret = os.environ.get("CONSUMER_SECRET_WOBA")
+access_token = os.environ.get("ACCESS_TOKEN_WOBA")
+access_token_secret = os.environ.get("ACCESS_TOKEN_SECRET_WOBA")
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
@@ -100,10 +100,10 @@ api_woba = tweepy.API(auth, wait_on_rate_limit = True)
 
 #Majin
 
-consumer_key = os.environ["CONSUMER_KEY_MAJ"]
-consumer_secret = os.environ["CONSUMER_SECRET_MAJ"]
-access_token = os.environ["ACCESS_TOKEN_MAJ"]
-access_token_secret = os.environ["ACCESS_TOKEN_SECRET_MAJ"]
+consumer_key = os.environ.get("CONSUMER_KEY_MAJ")
+consumer_secret = os.environ.get("CONSUMER_SECRET_MAJ")
+access_token = os.environ.get("ACCESS_TOKEN_MAJ")
+access_token_secret = os.environ.get("ACCESS_TOKEN_SECRET_MAJ")
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
@@ -114,10 +114,10 @@ api_maj = tweepy.API(auth, wait_on_rate_limit = True)
 
 #Zelander
 
-consumer_key = os.environ["CONSUMER_KEY_ZELD"]
-consumer_secret = os.environ["CONSUMER_SECRET_ZELD"]
-access_token = os.environ["ACCESS_TOKEN_ZELD"]
-access_token_secret = os.environ["ACCESS_TOKEN_SECRET_ZELD"]
+consumer_key = os.environ.get("CONSUMER_KEY_ZELD")
+consumer_secret = os.environ.get("CONSUMER_SECRET_ZELD")
+access_token = os.environ.get("ACCESS_TOKEN_ZELD")
+access_token_secret = os.environ.get("ACCESS_TOKEN_SECRET_ZELD")
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
@@ -128,10 +128,10 @@ api_zeld = tweepy.API(auth, wait_on_rate_limit = True)
 
 #Luffza
 
-consumer_key = os.environ["CONSUMER_KEY_UFF"]
-consumer_secret = os.environ["CONSUMER_SECRET_UFF"]
-access_token = os.environ["ACCESS_TOKEN_UFF"]
-access_token_secret = os.environ["ACCESS_TOKEN_SECRET_UFF"]
+consumer_key = os.environ.get("CONSUMER_KEY_UFF")
+consumer_secret = os.environ.get("CONSUMER_SECRET_UFF")
+access_token = os.environ.get("ACCESS_TOKEN_UFF")
+access_token_secret = os.environ.get("ACCESS_TOKEN_SECRET_UFF")
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
