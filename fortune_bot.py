99eedce75ac1891895801a510a7beb012f1b1fe0
Yuri <48300685+Rilufi@users.noreply.github.com>
2022-11-14 11:51:44 -0300

    Update fortune_bot.py

--- fortune_bot.py
+++ fortune_bot.py
@@ -2,8 +2,10 @@ import tweepy
 import fortune
 import os
 from auth import api_xame, api_uva, api_mevu, api, api_zark, api_lufi, api_woba, api_maj, api_zeld, api_uff
+from twitter_unfollow import unfollow
 
 
+unfollow = unfollow()
 bots = [api_xame, api_uva, api_mevu, api, api_zark, api_lufi, api_woba, api_maj, api_zeld, api_uff]
 
 def une(api):
@@ -19,3 +21,5 @@ def une(api):
 		
 for bot in bots:
 	une(bot)
+for bot in bots:
+	unfollow.unfollow(bot)
