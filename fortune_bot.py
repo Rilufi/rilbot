009a711ff5d25909c3876978bab5facd240f5d80
Yuri <48300685+Rilufi@users.noreply.github.com>
2022-11-14 14:42:17 -0300

    Update fortune_bot.py

--- fortune_bot.py
+++ fortune_bot.py
@@ -17,8 +17,21 @@ def une(api):
 	else:
 		print(f"sem fortuna p/ {my_screen_name} hoje")
 		pass
-		
+	
+def start_stream():
+    while True:
+        try:
+            sapi = tweepy.streaming.Stream(auth, CustomStreamListener(api))
+            sapi.filter(track=["Samsung", "s4", "s5", "note" "3", "HTC", "Sony", "Xperia", "Blackberry", "q5", "q10", "z10", "Nokia", "Lumia", "Nexus", "LG", "Huawei", "Motorola"])
+        except: 
+            continue
+
+	
+	
 for bot in bots:
 #	une(bot)
 #for bot in bots:
-	unfollow(bot)
+	try:
+		unfollow(bot)
+	except:
+		start_stream()
