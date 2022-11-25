9db888ade014f5f45a963e6714936fe0df1040bb
Yuri <48300685+Rilufi@users.noreply.github.com>
2022-11-25 14:29:22 -0300

    Update auth.py

--- alts/woba/auth.py
+++ alts/woba/auth.py
@@ -1,12 +1,13 @@
 import tweepy
 import os
 
-#Wobafab
 
-consumer_key = os.environ["CONSUMER_KEY_WOBA"]
-consumer_secret = os.environ["CONSUMER_SECRET_WOBA"]
-access_token = os.environ["ACCESS_TOKEN_WOBA"]
-access_token_secret = os.environ["ACCESS_TOKEN_SECRET_WOBA"]
+#Woba
+
+consumer_key = os.environ.get("CONSUMER_KEY_WOBA")
+consumer_secret = os.environ.get("CONSUMER_SECRET_WOBA")
+access_token = os.environ.get("ACCESS_TOKEN_WOBA")
+access_token_secret = os.environ.get("ACCESS_TOKEN_SECRET_WOBA")
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
