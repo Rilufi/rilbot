6658e34f3f57f578ce1dc393cd661a85489f336f
Yuri <48300685+Rilufi@users.noreply.github.com>
2022-11-25 14:21:17 -0300

    Update auth.py

--- alts/uff/auth.py
+++ alts/uff/auth.py
@@ -1,13 +1,12 @@
 import tweepy
 import os
 
-#Luffza
+#Uff
 
-consumer_key = os.environ["CONSUMER_KEY_UFF"]
-consumer_secret = os.environ["CONSUMER_SECRET_UFF"]
-access_token = os.environ["ACCESS_TOKEN_UFF"]
-access_token_secret = os.environ["ACCESS_TOKEN_SECRET_UFF"]
-
+consumer_key = os.environ.get("CONSUMER_KEY_UFF")
+consumer_secret = os.environ.get("CONSUMER_SECRET_UFF")
+access_token = os.environ.get("ACCESS_TOKEN_UFF")
+access_token_secret = os.environ.get("ACCESS_TOKEN_SECRET_UFF")
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
