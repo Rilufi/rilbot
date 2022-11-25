51ba1566b211903f553f9bac887ad61dd71e9ece
Yuri <48300685+Rilufi@users.noreply.github.com>
2022-11-25 14:30:17 -0300

    Update auth.py

--- alts/xame/auth.py
+++ alts/xame/auth.py
@@ -1,12 +1,13 @@
 import tweepy
 import os
 
-#Xamexavu
 
-consumer_key = os.environ["CONSUMER_KEY_XAME"]
-consumer_secret = os.environ["CONSUMER_SECRET_XAME"]
-access_token = os.environ["ACCESS_TOKEN_XAME"]
-access_token_secret = os.environ["ACCESS_TOKEN_SECRET_XAME"]
+#Xame
+
+consumer_key = os.environ.get("CONSUMER_KEY_XAME")
+consumer_secret = os.environ.get("CONSUMER_SECRET_XAME")
+access_token = os.environ.get("ACCESS_TOKEN_XAME")
+access_token_secret = os.environ.get("ACCESS_TOKEN_SECRET_XAME")
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
