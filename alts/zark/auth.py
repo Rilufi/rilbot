dcdd05b7d0a80423785c3e3e00dfbbd53c5c81a1
Yuri <48300685+Rilufi@users.noreply.github.com>
2022-11-25 14:31:06 -0300

    Update auth.py

--- alts/zark/auth.py
+++ alts/zark/auth.py
@@ -1,14 +1,12 @@
 import tweepy
 import os
 
-#Zarknall
+#Zark
 
-
-consumer_key = os.environ["CONSUMER_KEY_ZARK"]
-consumer_secret = os.environ["CONSUMER_SECRET_ZARK"]
-access_token = os.environ["ACCESS_TOKEN_ZARK"]
-access_token_secret = os.environ["ACCESS_TOKEN_SECRET_ZARK"]
-
+consumer_key = os.environ.get("CONSUMER_KEY_ZARK")
+consumer_secret = os.environ.get("CONSUMER_SECRET_ZARK")
+access_token = os.environ.get("ACCESS_TOKEN_ZARK")
+access_token_secret = os.environ.get("ACCESS_TOKEN_SECRET_ZARK")
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
