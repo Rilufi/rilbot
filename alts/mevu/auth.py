8dc0b07dd5986e0965de52eb63c18decad8efb06
Yuri <48300685+Rilufi@users.noreply.github.com>
2022-11-25 14:20:26 -0300

    Update auth.py

--- alts/mevu/auth.py
+++ alts/mevu/auth.py
@@ -1,13 +1,12 @@
 import tweepy
 import os
 
-#Mevuxa
+#Mevu
 
-consumer_key = os.environ["CONSUMER_KEY_MEVU"]
-consumer_secret = os.environ["CONSUMER_SECRET_MEVU"]
-access_token = os.environ["ACCESS_TOKEN_MEVU"]
-access_token_secret = os.environ["ACCESS_TOKEN_SECRET_MEVU"]
-
+consumer_key = os.environ.get("CONSUMER_KEY_MEVU")
+consumer_secret = os.environ.get("CONSUMER_SECRET_MEVU")
+access_token = os.environ.get("ACCESS_TOKEN_MEVU")
+access_token_secret = os.environ.get("ACCESS_TOKEN_SECRET_MEVU")
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
