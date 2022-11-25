6a96ac1250b46be459b35c4d19e5f0e435b92467
Yuri <48300685+Rilufi@users.noreply.github.com>
2022-11-25 14:18:39 -0300

    Update auth.py

--- alts/maj/auth.py
+++ alts/maj/auth.py
@@ -3,10 +3,10 @@ import os
 
 # Majin
 
-consumer_key = os.environ["CONSUMER_KEY_MAJ"]
-consumer_secret = os.environ["CONSUMER_SECRET_MAJ"]
-access_token = os.environ["ACCESS_TOKEN_MAJ"]
-access_token_secret = os.environ["ACCESS_TOKEN_SECRET_MAJ"]
+consumer_key = os.environ.get("CONSUMER_KEY_MAJ")
+consumer_secret = os.environ.get("CONSUMER_SECRET_MAJ")
+access_token = os.environ.get("ACCESS_TOKEN_MAJ")
+access_token_secret = os.environ.get("ACCESS_TOKEN_SECRET_MAJ")
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
