37092dde451a96d75f6d965184908ee92e263ce3
Yuri <48300685+Rilufi@users.noreply.github.com>
2022-11-25 14:22:15 -0300

    Update auth.py

--- alts/uva/auth.py
+++ alts/uva/auth.py
@@ -1,14 +1,12 @@
 import tweepy
 import os
 
-#Uvaxemax
+#Uva
 
-
-consumer_key = os.environ["CONSUMER_KEY_UVA"]
-consumer_secret = os.environ["CONSUMER_SECRET_UVA"]
-access_token = os.environ["ACCESS_TOKEN_UVA"]
-access_token_secret = os.environ["ACCESS_TOKEN_SECRET_UVA"]
-
+consumer_key = os.environ.get("CONSUMER_KEY_UVA")
+consumer_secret = os.environ.get("CONSUMER_SECRET_UVA")
+access_token = os.environ.get("ACCESS_TOKEN_UVA")
+access_token_secret = os.environ.get("ACCESS_TOKEN_SECRET_UVA")
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
