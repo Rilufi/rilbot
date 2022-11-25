16448d240ad3dbc8dd1f86ffc4927497cc51ec5f
Yuri <48300685+Rilufi@users.noreply.github.com>
2022-11-25 14:32:07 -0300

    Update auth.py

--- alts/zeld/auth.py
+++ alts/zeld/auth.py
@@ -1,13 +1,12 @@
 import tweepy
 import os
 
-#Zelander
+#Zeld
 
-consumer_key = os.environ["CONSUMER_KEY_ZELD"]
-consumer_secret = os.environ["CONSUMER_SECRET_ZELD"]
-access_token = os.environ["ACCESS_TOKEN_ZELD"]
-access_token_secret = os.environ["ACCESS_TOKEN_SECRET_ZELD"]
-
+consumer_key = os.environ.get("CONSUMER_KEY_ZELD")
+consumer_secret = os.environ.get("CONSUMER_SECRET_ZELD")
+access_token = os.environ.get("ACCESS_TOKEN_ZELD")
+access_token_secret = os.environ.get("ACCESS_TOKEN_SECRET_ZELD")
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
