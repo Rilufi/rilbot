9cdcdbd1208dec9abdfd8914877f813bb3cf6d70
Yuri <48300685+Rilufi@users.noreply.github.com>
2022-11-26 11:52:23 -0300

    Update twitter_bot.py

--- alts/uva/twitter_bot.py
+++ alts/uva/twitter_bot.py
@@ -12,7 +12,7 @@ day = datetime.today().day
 # Run script
 if day in [1, 6, 11, 16, 21, 26]:
   print("Uva is on vacation.")
-  tweetbot.read('Freud_Dream_Psychology.txt')
+  tweetbot.read('alts/Freud_Dream_Psychology.txt')
   my_first_text = tweetbot.generate_text(25, seedword=[u'dream', u'psychoanalysis'])
   api.update_status('"'+my_first_text+'"')
 else:
