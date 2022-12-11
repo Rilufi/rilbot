5d3027f528d72aacf65cfcc8586fc1dd2532a8a0
Yuri <48300685+Rilufi@users.noreply.github.com>
2022-12-11 11:29:20 -0300

    Update fortune_bot.py

--- fortune_bot.py
+++ fortune_bot.py
@@ -1,7 +1,7 @@
 import tweepy
 import fortune
 import os
-from auth import api_xame, api_uva, api_mevu, api, api_zark, api_lufi, api_woba, api_maj, api_zeld, api_uff
+from auth import api_xame, api_uva, api_mevu, api, api_zark, api_lufi, api_woba, api_maj, api_zeld
 #from twitter_unfollow import unfollow
 
 
