74ccda9839d6c23794e2eb281ac7e652df5c4700
Yuri <xamexavu@hotmail.com>
2023-06-14 15:09:48 -0300

    Update auth.py

--- alts/mevu/auth.py
+++ alts/mevu/auth.py
@@ -1,15 +1,23 @@
 import tweepy
 import os
 
+api = tweepy.Client(
+    consumer_key = os.environ.get("CONSUMER_KEY_MEVU"),
+    consumer_secret = os.environ.get("CONSUMER_SECRET_MEVU"),
+    access_token = os.environ.get("ACCESS_TOKEN_MEVU"),
+    access_token_secret = os.environ.get("ACCESS_TOKEN_SECRET_MEVU")
+)
+
+
 #Mevu
 
-consumer_key = os.environ.get("CONSUMER_KEY_MEVU")
-consumer_secret = os.environ.get("CONSUMER_SECRET_MEVU")
-access_token = os.environ.get("ACCESS_TOKEN_MEVU")
-access_token_secret = os.environ.get("ACCESS_TOKEN_SECRET_MEVU")
+#consumer_key = os.environ.get("CONSUMER_KEY_MEVU")
+#consumer_secret = os.environ.get("CONSUMER_SECRET_MEVU")
+#access_token = os.environ.get("ACCESS_TOKEN_MEVU")
+#access_token_secret = os.environ.get("ACCESS_TOKEN_SECRET_MEVU")
 
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
+#auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+#auth.set_access_token(access_token, access_token_secret)
 
 # Controls Twitter account
-api = tweepy.API(auth, wait_on_rate_limit = True)
+#api = tweepy.API(auth, wait_on_rate_limit = True)
