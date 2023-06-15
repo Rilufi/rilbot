b75022e5c6a61f2fec49ac1c8502070c47c22952
Yuri <xamexavu@hotmail.com>
2023-06-15 12:34:41 -0300

    Update fortune_bot.py

--- fortune_bot.py
+++ fortune_bot.py
@@ -7,15 +7,18 @@ from auth import api_xame, api_uva, api_mevu, api, api_zark, api_lufi, api_maj, 
 
 bots = [api_xame, api_uva, api_mevu, api, api_zark, api_lufi, api_maj, api_zeld]
 
+count = 0
+
 def une(api):
 	fortune = os.popen("fortune alts/fortunes").read()
-	my_screen_name = api.me().screen_name
+	count += 1
+#	my_screen_name = api.me().screen_name
 #	while len(fortune) < 280:
 	if len(fortune) < 280:
-		api.update_status(fortune)
-		print(f"fortuna p/ {my_screen_name} rolou")
+		api.create_tweet(text = fortune)
+		print(f"fortuna {count} rolou")
 	else:
-		print(f"sem fortuna p/ {my_screen_name} hoje")
+		print(f"fortuna {count} não rolou")
 		pass
 	
 	
