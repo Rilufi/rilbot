ef6731d59248edcd45560dcbdd197ea4fc805b28
Yuri <xamexavu@hotmail.com>
2023-06-19 15:42:02 -0300

    Update auth.py

--- alts/lufi/auth.py
+++ alts/lufi/auth.py
@@ -3,9 +3,9 @@ import os
 
 #Lufi
 api = tweepy.Client(
-consumer_key = os.environ.get("CONSUMER_KEY_LUFI")
-consumer_secret = os.environ.get("CONSUMER_SECRET_LUFI")
-access_token = os.environ.get("ACCESS_TOKEN_LUFI")
+consumer_key = os.environ.get("CONSUMER_KEY_LUFI"),
+consumer_secret = os.environ.get("CONSUMER_SECRET_LUFI"),
+access_token = os.environ.get("ACCESS_TOKEN_LUFI"),
 access_token_secret = os.environ.get("ACCESS_TOKEN_SECRET_LUFI")
 )
 #auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
