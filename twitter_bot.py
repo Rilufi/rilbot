0d72fa1756b9ecc1947b0c21a8dc8fa9d34650d8
Yuri <xamexavu@hotmail.com>
2023-06-19 14:52:04 -0300

    Update twitter_bot.py

--- twitter_bot.py
+++ twitter_bot.py
@@ -1,16 +1,17 @@
 # Run: py twitter_bot.py
 from auth import api
 from datetime import datetime
-from twitter_winner import winner
+#from twitter_winner import winner
 
 # Create class instance
-win = winner()
+#win = winner()
 day = datetime.today().day
 
 # Run script
 if day in [1, 6, 11, 16, 21, 26]:
   print("Rilufix is auditioning to be cast in Asterix and Obelix next movie.")
 else:
-  win.favorite_follow_retweet()
+  pass
+#  win.favorite_follow_retweet()
 
-print(win.sort_file('twitterFilter.txt') + '\n')
+#print(win.sort_file('twitterFilter.txt') + '\n')
