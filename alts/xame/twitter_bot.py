7d271d78a794ab10e72bfd6efcf481633d3c8f5b
Yuri <xamexavu@hotmail.com>
2023-06-19 15:38:52 -0300

    Update twitter_bot.py

--- alts/xame/twitter_bot.py
+++ alts/xame/twitter_bot.py
@@ -1,19 +1,20 @@
 # Run: py twitter_bot.py
 from auth import api
 from datetime import datetime
-from twitter_winner import winner
-from xame import rtquery
+#from twitter_winner import winner
+#from xame import rtquery
 
 # Create class instance
-win = winner()
-rt = rtquery()
+#win = winner()
+#rt = rtquery()
 day = datetime.today().day
 
 # Run script
 if day in [3, 8, 13, 18, 23, 28]:
   print("Xamexavu is meditating today.")
-  rt.rtquery()
+#  rt.rtquery()
 else:
-  win.favorite_follow_retweet()
+  pass
+ # win.favorite_follow_retweet()
 
-print(win.sort_file('twitterFilter.txt') + '\n')
+#print(win.sort_file('twitterFilter.txt') + '\n')
