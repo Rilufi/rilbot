fa14be11e7222f24e1f8e007cccd132bdc11bceb
Yuri <xamexavu@hotmail.com>
2023-06-19 15:31:59 -0300

    Update twitter_bot.py

--- alts/lufi/twitter_bot.py
+++ alts/lufi/twitter_bot.py
@@ -1,11 +1,11 @@
 # Run: py twitter_bot.py
 from auth import api
 from datetime import datetime
-from twitter_winner import winner
+#from twitter_winner import winner
 from lufi import quoter
 
 # Create class instance
-win = winner()
+#win = winner()
 quoter = quoter()
 day = datetime.today().day
 
@@ -14,6 +14,7 @@ if day in [2, 7, 12, 17, 22, 27]:
   print("Lufilir don't work today.")
   quoter.tweet_quote()
 else:
-  win.favorite_follow_retweet()
+  pass
+#  win.favorite_follow_retweet()
 
 print(win.sort_file('twitterFilter.txt') + '\n')
