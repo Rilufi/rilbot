5a1cb0ca49a27890041a3d0ae6b29a0850d64b57
Yuri <xamexavu@hotmail.com>
2023-06-19 15:36:41 -0300

    Update twitter_bot.py

--- alts/zeld/twitter_bot.py
+++ alts/zeld/twitter_bot.py
@@ -1,19 +1,19 @@
 # Run: py twitter_bot.py
 from auth import api
 from datetime import datetime
-from twitter_winner import winner
-from zeld import rtquery
+#from twitter_winner import winner
+#from zeld import rtquery
 
 # Create class instance
-win = winner()
-rt = rtquery()
+#win = winner()
+#rt = rtquery()
 day = datetime.today().day
 
 # Run script
 if day in [5, 10, 15, 20, 25, 30]:
   print("Zelda doesn't even care about Link.")
-  rt.rtquery()
+#  rt.rtquery()
 else:
-  win.favorite_follow_retweet()
+#  win.favorite_follow_retweet()
 
-print(win.sort_file('twitterFilter.txt') + '\n')
+#print(win.sort_file('twitterFilter.txt') + '\n')
