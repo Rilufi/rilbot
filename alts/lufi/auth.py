419f97dd55531f7773a43a760ce2fc1bd72a3b94
Yuri <xamexavu@hotmail.com>
2023-06-19 15:33:02 -0300

    Update auth.py

--- alts/lufi/auth.py
+++ alts/lufi/auth.py
@@ -2,14 +2,14 @@ import tweepy
 import os
 
 #Lufi
-
+api = tweepy.Client(
 consumer_key = os.environ.get("CONSUMER_KEY_LUFI")
 consumer_secret = os.environ.get("CONSUMER_SECRET_LUFI")
 access_token = os.environ.get("ACCESS_TOKEN_LUFI")
 access_token_secret = os.environ.get("ACCESS_TOKEN_SECRET_LUFI")
-
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
+)
+#auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+#auth.set_access_token(access_token, access_token_secret)
 
 # Controls Twitter account
-api = tweepy.API(auth, wait_on_rate_limit = True)
+#api = tweepy.API(auth, wait_on_rate_limit = True)
