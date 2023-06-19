447ec1c20c8891d56735ec8118d6632e5224a4b6
Yuri <xamexavu@hotmail.com>
2023-06-19 15:33:41 -0300

    Update twitter_bot.py

--- alts/maj/twitter_bot.py
+++ alts/maj/twitter_bot.py
@@ -1,19 +1,19 @@
 # Run: py twitter_bot.py
 from auth import api
 from datetime import datetime
-from twitter_winner import winner
-from maj import rtquery
+#from twitter_winner import winner
+#from maj import rtquery
 
 # Create class instance
-win = winner()
-rt = rtquery()
+#win = winner()
+#rt = rtquery()
 day = datetime.today().day
 
 # Run script
 if day in [3, 8, 13, 18, 23, 28]:
   print("Majin is in a really rage right now.")
-  rt.rtquery()
+#  rt.rtquery()
 else:
-  win.favorite_follow_retweet()
+#  win.favorite_follow_retweet()
 
-print(win.sort_file('twitterFilter.txt') + '\n')
+#print(win.sort_file('twitterFilter.txt') + '\n')
