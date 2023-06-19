12c0ee404a42aa821026a8fd5eb1df7f20b5ab68
Yuri <xamexavu@hotmail.com>
2023-06-19 15:43:48 -0300

    Update auth.py

--- alts/zark/auth.py
+++ alts/zark/auth.py
@@ -3,7 +3,7 @@ import os
 
 #Zark
 
-api = tweppy.Client(
+api = tweepy.Client(
 consumer_key = os.environ.get("CONSUMER_KEY_ZARK"),
 consumer_secret = os.environ.get("CONSUMER_SECRET_ZARK"),
 access_token = os.environ.get("ACCESS_TOKEN_ZARK"),
