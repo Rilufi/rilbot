d1623dccfd7e698ce2536cb7e040a3be7d8283d8
Yuri <xamexavu@hotmail.com>
2023-06-19 15:40:56 -0300

    Update twitter_bot.py

--- alts/uva/twitter_bot.py
+++ alts/uva/twitter_bot.py
@@ -1,11 +1,11 @@
 # Run: py twitter_bot.py
 from auth import api
 from datetime import datetime
-from twitter_winner import winner
+#from twitter_winner import winner
 from uva import MarkovBot
 
 # Create class instance
-win = winner()
+#win = winner()
 tweetbot = MarkovBot()
 day = datetime.today().day
 
@@ -14,8 +14,9 @@ if day in [1, 6, 11, 16, 21, 26]:
   print("Uva is on vacation.")
   tweetbot.read('alts/Freud_Dream_Psychology.txt')
   my_first_text = tweetbot.generate_text(25, seedword=[u'dream', u'psychoanalysis'])
-  api.update_status('"'+my_first_text+'"')
+  api.create_tweet(text = '"'+my_first_text+'"')
 else:
-  win.favorite_follow_retweet()
+  pass
+#  win.favorite_follow_retweet()
 
-print(win.sort_file('twitterFilter.txt') + '\n')
+#print(win.sort_file('twitterFilter.txt') + '\n')
