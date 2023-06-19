fef36abad6f2c6085b547d1af0c2d2901a9819b0
Yuri <xamexavu@hotmail.com>
2023-06-19 15:34:34 -0300

    Update twitter_bot.py

--- alts/mevu/twitter_bot.py
+++ alts/mevu/twitter_bot.py
@@ -1,11 +1,11 @@
 # Run: py twitter_bot.py
 from auth import api
 from datetime import datetime
-from twitter_winner import winner
+#from twitter_winner import winner
 from mevu import fact
 
 # Create class instance
-win = winner()
+#win = winner()
 fact = fact()
 day = datetime.today().day
 
@@ -14,6 +14,7 @@ if day in [4, 9, 14, 19, 24, 29]:
   print("Mevu is not home today.")
   fact.fact()
 else:
-  win.favorite_follow_retweet()
+  pass
+#  win.favorite_follow_retweet()
 
-print(win.sort_file('twitterFilter.txt') + '\n')
+#print(win.sort_file('twitterFilter.txt') + '\n')
