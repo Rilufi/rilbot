d03e279812de57114b54bc2fd50b9785011dbfa3
Yuri <xamexavu@hotmail.com>
2023-06-19 15:20:38 -0300

    Update twitter_bot.py

--- twitter_bot.py
+++ twitter_bot.py
@@ -11,6 +11,7 @@ day = datetime.today().day
 if day in [1, 6, 11, 16, 21, 26]:
   print("Rilufix is auditioning to be cast in Asterix and Obelix next movie.")
 else:
+  print("Need to find something for Rilufix to do")
   pass
 #  win.favorite_follow_retweet()
 
